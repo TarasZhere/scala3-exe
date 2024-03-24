d36460203fb649d033fc755226839fbcbd3b046c
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2024-03-23 21:44:35 -0400

    completed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Exercise
-Write a Scala program to print "Hello, world" and version of the Scala language.
+Write a Scala program to compute the sum of the two given integer values. If the two values are the same, then return triples their sum.
 
 ## Run the solution
 Run the shell command in a local terminal
--- solution.scala
+++ solution.scala
@@ -1,3 +1,11 @@
 @main
 def main():Unit =
-    println("Hello World!")
+    val weirdSum: (Int, Int) => Int = (x:Int, y:Int) => if x == y then x * 3 else x + y
+
+    val numbers:List[(Int, Int)] = List(
+        (4, 5),
+        (5, 5),
+        (9, 2)
+    )
+    for number <- numbers do
+        println(weirdSum(number(0), number(1)))
